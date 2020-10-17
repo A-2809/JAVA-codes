bc66e312961fd2e17724a5aa531eae2fd7e22821
A-2809 <66512619+A-2809@users.noreply.github.com>
2020-10-17 22:00:37 +0530

    array

---
+++ Array.java
@@ -0,0 +1,22 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Output looks like: ");
+		int i,j;
+		int a[][]= {{1,3,4},{2,4,6},{3,4,5}};
+		int b[][]= {{1,3,4},{2,4,6},{3,4,5}};
+		for(i=0;i<3;i++) {
+			for(j=0;j<3;j++)
+			{
+				 System.out.print(a[i][j] + b[i][j]);
+				 System.out.print(" ");
+				
+			}
+			System.out.println();
+		}
+		System.out.println("ALL THE BEST!!!");
+	}
+
+}
